9a72a9d888988a74f0fc025dfbdbe55d8c8a0b23
mayur <128782100+mayurcoder2003@users.noreply.github.com>
2026-01-04 19:58:40 +0530

    Add files via upload

---
+++ Admin Login/Connection.php
@@ -0,0 +1,22 @@
+<?php
+
+$server="localhost";
+
+$username="root";
+
+$pass="";
+
+$db="result_analysis";
+
+
+$con= new Mysqli($server,$username,$pass,$db);
+
+if($con->connect_error){
+
+    die("Connection Failed".$con->connect_error);
+
+}
+
+
+?>
+
